49614e9a934baee2a9c3690310f86a504341e57b
Alexey Nikolaev <leshanikolaev2004@yandex.ru>
2022-11-11 23:27:58 +0300

    Lab 10 was added

---
+++ LAB 10/Program 10/Fifth_task.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+void delSpaces(char *string, char *stringRes) {
+    if (*string == '\0') return;     //выход из рекурсси
+
+    int i = strcspn(string, " ");   //Находим длинну строку до первого пробела
+    strncat(stringRes, string, i);
+    char lastEl = stringRes[strlen(stringRes) - 1];
+    char *newString = &string[i];   //Указатель на строку начинающуюся с первого пробела
+
+    if (lastEl == '(') {
+        while (newString[0] == ' ') newString += 1;  //Записываем новое начало строки (убираем пробелы)
+    }
+
+    if (lastEl == '.') {    //Удаляет пробелы после последней точки
+        if (newString[0] == ' ' && newString[1] == ' ' && newString[2] == ' ' && newString[3] == '\0') return;
+        else if (newString[0] == ' ' && newString[1] == ' ' && newString[2] == '\0') return;
+        else if (newString[0] == ' ' && newString[1] == '\0') return;
+    }
+
+    if (newString[0] == ' ' && newString[1] == ' ' && newString[2] == ' ') {
+        while (newString[2] == ' ') newString = newString + 1;
+            strncat(stringRes, newString, 2);
+            newString += 2;
+    } else {
+        strncat(stringRes, newString, 1);
+        newString += 1;
+    }
+
+    delSpaces(newString, stringRes);    //Снова вызов функции для правой части строки с пробелами
+}
+
+int main() {
+    char string[100] = "", stringRes[100] = "";
+    printf("Insert string:");
+    gets(string);
+    delSpaces(string, stringRes);
+    printf("Result string: \n%s", stringRes);
+    return 0;
+}
---
+++ LAB 10/Program 10/second_task.c
@@ -0,0 +1,48 @@
+/*
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+typedef struct {
+    double x;
+    double y;
+} Coords;
+
+double **distances(Coords *arr,  int n) {
+    double **dist = malloc(sizeof(double *) * n);
+    for (int i = 0; i < n; ++i) dist[i] = malloc(sizeof(double) * (n-1));
+    for (int i = 0; i < n; ++i) {
+        int count = 0;
+        for (int j = 0; j < n; ++j) {
+            if (j == i) continue;
+            dist[i][count] = sqrt((arr[i].x - arr[j].x) * (arr[i].x - arr[j].x) + (arr[i].y - arr[j].y) * (arr[i].y - arr[j].y));
+            count++;
+        }
+    }
+    return dist;
+}
+
+int main() {
+    int n;
+    printf("Insert the number of points:");
+    scanf("%d", &n);
+    Coords arr[n];
+    for (int i = 0; i < n; ++i) {
+        printf("Insert X for the %d point:", i+1);
+        scanf("%lf", &arr[i].x);
+        printf("Insert Y for the %d point:", i+1);
+        scanf("%lf", &arr[i].y);
+    }
+    double **distArr = distances(arr, n);
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n-1; ++j) {
+            printf("%.2lf ", distArr[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
+*/
