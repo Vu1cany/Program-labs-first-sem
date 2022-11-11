70d0c8e658d600422c51ad93e3740192658853ed
Alexey Nikolaev <leshanikolaev2004@yandex.ru>
2022-11-11 13:42:51 +0300

    Lab 9 was added

---
+++ LAB 9/Program 9/fifth_task.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int monthsNum;
+    double percent, capital;
+    printf("Insert the number of months:");
+    scanf("%d", &monthsNum);
+    printf("Enter the number of percentages:");
+    scanf("%lf", &percent);
+    printf("Enter the initial amount of money:");
+    scanf("%lf", &capital);
+    double monthPercent = 1 + ((percent / 12) / 100);
+    for (int i = 1; i <= monthsNum; ++i) {
+        capital*= monthPercent;
+        printf("Capital = %.2lf after %d months\n", capital, i);
+    }
+
+    return 0;
+}
---
+++ LAB 9/Program 9/second_task.c
@@ -0,0 +1,21 @@
+/*#include <stdio.h>
+
+int main() {
+    int time, subFee, costMinute;
+    printf("Enter the duration of the conversations:");
+    scanf("%d", &time);
+    printf("Enter the amount of the subscription fee:");
+    scanf("%d", &subFee);
+    printf("Enter the cost of a minute over the limit:");
+    scanf("%d", &costMinute);
+    short limit = 499;
+    int fullCost = subFee;
+
+    if (time > limit) {
+        fullCost+= (time - limit)*costMinute;
+    }
+
+    printf("Monthly maintenance cost: %d", fullCost);
+
+    return 0;
+}*/
