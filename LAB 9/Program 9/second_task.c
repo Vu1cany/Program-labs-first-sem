8cf536fe43c638776058b083befe6feaa2e82b89
Alexey Nikolaev <leshanikolaev2004@yandex.ru>
2022-11-15 18:56:35 +0300

    Lab 9 was fixed

--- LAB 9/Program 9/second_task.c
+++ LAB 9/Program 9/second_task.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    int time, subFee, costMinute;
+    int time, subFee, costMinute = 0;
     printf("Enter the duration of the conversations:");
     scanf("%d", &time);
     printf("Enter the amount of the subscription fee:");
     scanf("%d", &subFee);
-    printf("Enter the cost of a minute over the limit:");
-    scanf("%d", &costMinute);
     short limit = 499;
+    while (costMinute <= subFee/limit) {
+        printf("Enter the cost of a minute over the limit:");
+        scanf("%d", &costMinute);
+    }
+
     int fullCost = subFee;
 
     if (time > limit) {
         fullCost += (time - limit) * costMinute;
     }
+
+
 
     printf("Monthly maintenance cost: %d", fullCost);
 
