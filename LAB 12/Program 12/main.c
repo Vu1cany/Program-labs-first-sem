2483fda6f7b53ce66bc27b44121c762df69c75c0
Alexey Nikolaev <leshanikolaev2004@yandex.ru>
2022-11-15 11:54:04 +0300

    Lab 12 was added

--- LAB 12/Program 12/main.c
+++ LAB 12/Program 12/main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char **argv) {
+
+    for (int i = 1; i < argc; ++i) {
+        int err = mkdir(argv[i], 0777);
+        if (!err) {
+            printf("Directory %s was created successfully!\n", argv[i]);
+        } else {
+            printf("Directory %s is already exists!\n", argv[i]);
+        }
+    }
+
+    FILE *file = fopen("Info.txt", "w");
+    
+    DIR *dir;
+    struct dirent *current;
+    dir = opendir(".");
+    int count = 0;
+    while ((current = readdir(dir)) != NULL) {
+        if (current->d_type == DT_DIR) {
+            count++;
+            if (count < 3) continue;
+            fputs(current->d_name, file);
+            fputs("\n", file);
+        }
+    }
+
+    fclose(file);
+    closedir(dir);
+
     return 0;
 }
